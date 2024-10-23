25bc17c148d48a27aa8d44e6fb29bc6357acaac6
vladislav_nerubashchenko <blacktales911@yandex.ru>
2024-10-23 21:48:53 +0300

    Nothing

---
+++ java/GlebusWebAppJavaPart/src/main/appearance-service/src/main/java/com/example/appearanceservice/AppearanceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.appearanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppearanceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppearanceServiceApplication.class, args);
+	}
+
+}
