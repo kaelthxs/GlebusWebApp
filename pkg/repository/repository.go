f312b46819ff4aca7a07b4dfff6968b7f6fc356b
vladislav_nerubashchenko <blacktales911@yandex.ru>
2024-12-01 23:40:58 +0300

    add CRUD methods

--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -19,9 +19,28 @@ type Client interface {
 }
 
 type Album interface {
+	createAlbum(c *gin.Context)
+	getAllAlbum(c *gin.Context)
+	getAllAlbumsByRating(c *gin.Context)
+	getAlbumById(c *gin.Context)
+	updateAlbum(c *gin.Context)
+	deleteAlbum(c *gin.Context)
 }
 
 type Music interface {
+	createMusic(c *gin.Context)
+	getAllMusic(c *gin.Context)
+	getMusicById(c *gin.Context)
+	updateMusic(c *gin.Context)
+	deleteMusic(c *gin.Context)
+}
+
+type Review interface {
+	createReview(c *gin.Context)
+	getAllReview(c *gin.Context)
+	getReviewById(c *gin.Context)
+	updateReview(c *gin.Context)
+	deleteReview(c *gin.Context)
 }
 
 type Repository struct {
@@ -29,6 +48,7 @@ type Repository struct {
 	Client
 	Album
 	Music
+	Review
 }
 
 func NewRepository(db *sqlx.DB) *Repository {
--- pkg/service/service.go
+++ pkg/service/service.go
@@ -9,6 +9,7 @@ import (
 type Authorization interface {
 	CreateClient(client oooGlebusApi.Client) (int, error)
 	GenerateToken(username, password string) (string, error)
+	ParseToken(token string) (int, error)
 }
 
 type Client interface {
@@ -19,9 +20,28 @@ type Client interface {
 }
 
 type Album interface {
+	createAlbum(c *gin.Context)
+	getAllAlbum(c *gin.Context)
+	getAllAlbumsByRating(c *gin.Context)
+	getAlbumById(c *gin.Context)
+	updateAlbum(c *gin.Context)
+	deleteAlbum(c *gin.Context)
 }
 
 type Music interface {
+	createMusic(c *gin.Context)
+	getAllMusic(c *gin.Context)
+	getMusicById(c *gin.Context)
+	updateMusic(c *gin.Context)
+	deleteMusic(c *gin.Context)
+}
+
+type Review interface {
+	createReview(c *gin.Context)
+	getAllReview(c *gin.Context)
+	getReviewById(c *gin.Context)
+	updateReview(c *gin.Context)
+	deleteReview(c *gin.Context)
 }
 
 type Service struct {
@@ -29,6 +49,7 @@ type Service struct {
 	Client
 	Album
 	Music
+	Review
 }
 
 func NewService(repos *repository.Repository) *Service {
